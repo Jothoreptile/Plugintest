d08bd11093fc5e911cee7bece24dce43d95e9f39
Jothoreptile <113062793+Jothoreptile@users.noreply.github.com>
2023-06-02 15:49:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@
 #   v0.1 : First try to not broke everything
 
 # Create the plugintest folder if it doesn't exist
-mkdir -p ~/.printer_data/config/plugintest
+mkdir -p ~/.printer_data/config/Plugintest
 
 # Clone the GitHub repository
 git clone https://github.com/Jothoreptile/Plugintest
 
 # Copy the files to the Klipper configuration directory
-cp -r Plugintest/* ~/.printer_data/config/plugintest
+cp -r Plugintest/* ~/.printer_data/config/Plugintest
 
 function restart_klipper {
     echo "[POST-INSTALL] Restarting Klipper..."

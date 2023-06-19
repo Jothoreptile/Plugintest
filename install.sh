71c80b9e8ae4cc01a3c49d06e9b0de7c6383fe7d
Jothoreptile <113062793+Jothoreptile@users.noreply.github.com>
2023-06-19 11:54:42 +0200

    Update install.sh
    Test with chatgtp

--- install.sh
+++ install.sh
@@ -3,22 +3,33 @@
 ###### AUTOMATED INSTALL AND UPDATE SCRIPT ######
 #################################################
 # Written by Jotho with his little hand
-# @version: 0.2
 
-# CHANGELOG:
-#   v0.1 : First try to not broke everything
-#   v0.2 : Try to not brove V2
-#   V0.3 : Add Plugin.cfg into the good folder
+# Define variables
+PLUGIN_NAME="Plugintest"
+PLUGIN_REPO="https://github.com/Jothoreptile/Plugintest.git"
+PLUGIN_CONFIG_DIR="~/printer_data/config/$PLUGIN_NAME"
 
-# Create the plugintest folder if it doesn't exist
-$ mkdir -p ~/printer_data/config/Plugintest
+# Ensure the required dependencies are installed
+if ! command -v git >/dev/null 2>&1; then
+    echo "Git is not installed. Please install Git and try again."
+    exit 1
+fi
 
-#copy cfg files into printer data
-wget -P ~/printer_data/config/Plugintest https://raw.githubusercontent.com/Jothoreptile/Plugintest/main/Plugin.cfg
+# Check if the plugin directory exists
+if [ ! -d "$PLUGIN_CONFIG_DIR" ]; then
+    # Directory doesn't exist, clone the plugin repository
+    echo "Installing $PLUGIN_NAME..."
+    git clone "$PLUGIN_REPO" "$PLUGIN_CONFIG_DIR"
+else
+    # Directory exists, update the plugin repository
+    echo "Updating $PLUGIN_NAME..."
+    cd "$PLUGIN_CONFIG_DIR"
+    git pull
+fi
 
-function restart_klipper {
-    echo "[POST-INSTALL] Restarting Klipper..."
-    sudo systemctl restart klipper
-}
+# Restart Klipper service
+echo "Restarting Klipper..."
+sudo systemctl restart klipper
 
-echo "Installation complete!"
+echo "Installation and update complete!"
+
